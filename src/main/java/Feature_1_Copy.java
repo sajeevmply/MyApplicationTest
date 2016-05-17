3928f48a358fb21f2efb1312c6814bbd76d50dc0
sajeevmply <sajeevmply@gmail.com>
2016-05-17 16:14:20 +0530

    This is a feature1 branch commit

---
+++ src/main/java/Feature_1_Copy.java
@@ -0,0 +1,12 @@
+/**
+* This is a feature 2 file that is specific to the feature
+*/
+public Feature_2
+{
+	String feature = "This is feature 2";
+	public void displayValue()
+	{
+		Common common = new Common();
+		common.feature(feature);
+	}
+}
